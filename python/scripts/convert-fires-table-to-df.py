4d3f8367a78c5a3a92977849ec6f50a3d2153012
Nite Toad <dtorresjr1106@gmail.com>
2024-03-05 15:24:20 -0600

    updated notebooks, fixed prior errors as well

--- python/scripts/convert-fires-table-to-df.py
+++ python/scripts/convert-fires-table-to-df.py
@@ -1,12 +1,18 @@
+"""
+This script is meant to extract the table Fires from the database, using 
+pandas to make the query and sqlite3 to connect to the local database.
+"""
 # !/usr/bin/env python3
 import sqlite3
 import pandas as pd 
 
 # connect to the sqlite database
-conn = sqlite3.connect("data/Data/FPA_FOD_20210617.sqlite")
+conn = sqlite3.connect("python/data/unprocessed/FPA_FOD_20210617.sqlite")
 
-# execute a select statement
-df = pd.read_sql_query("SELECT * FROM Fires", conn)
+# execute a select statement to select Fires table from database
+df_fire = pd.read_sql_query("SELECT * FROM Fires", conn)
+df_other = pd.read_sql_query("SELECT * FROM NWCG_UnitIDActive_20200123", conn)
 
-df.to_csv('data/wildfire-dataset.csv', index=False)
-
+# select path to where the data from the fires table will be saved 
+df_fire.to_csv('python/data/unprocessed/wildfire-dataset_copy.csv', index=False)
+df_other.to_csv('python/data/unprocessed/NWCG_UnitIDActive_20200123_copy.csv', index=False)
--- python/scripts/view-sqlite.py
+++ python/scripts/view-sqlite.py
@@ -1,8 +1,12 @@
+"""
+This script works to see all tables available in the database using sqlite3.
+"""
 # !/usr/bin/env python3
 import sqlite3
+import pandas as pd
 
 # Connect to the SQLite database
-conn = sqlite3.connect('data/Data/FPA_FOD_20210617.sqlite')
+conn = sqlite3.connect('python/data/unprocessed/FPA_FOD_20210617.sqlite')
 
 # Create a cursor object
 cursor = conn.cursor()
@@ -14,5 +18,13 @@ cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
 table_names = cursor.fetchall()
 
 # Now, 'table_names' is a list of tuples where each tuple corresponds to a table name in the database
+print("\n")
+print("*"*80)
+print("Tables:")
 for table_name in table_names:
+    # view tables available
     print(table_name[0])
+
+df = pd.read_sql_query("select * from Fires;", conn)
+columns = (list(df.columns))
+print(f"\nFire columns: {columns}")
